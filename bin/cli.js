71746100d2e7eef7ed45dd829f4425abc5fb6615
leonard01 <leonard.collins1983@gmail.com>
2025-06-29 16:10:48 +0700

    added diff

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,11 @@
 
 const { program } = require('commander');
 const { initRepo } = require('../lib/init');
+const { commitSnapshot } = require('../lib/commit');
+const { listSnapshots } = require('../lib/list');
+const { diffSnapshots } = require('../lib/diff');
+
+
 
 program
   .command('init')
@@ -9,5 +14,29 @@ program
   .action(() => {
     initRepo();
   });
+
+  program
+  .command('commit <file>')
+  .option('-m, --message <message>', 'Commit message')
+  .description('Take a snapshot of the specified file')
+  .action((file, options) => {
+    commitSnapshot(file, options.message);
+  });
+
+  program
+  .command('list')
+  .description('List all saved snapshots')
+  .action(() => {
+    listSnapshots();
+  });
+
+  program
+  .command('diff <snapshotId> [file]')
+  .description('Compare a snapshot to the current file or another snapshot')
+  .action((id, file) => {
+    diffSnapshots(id, file);
+  });
+
+
 
 program.parse(process.argv);
